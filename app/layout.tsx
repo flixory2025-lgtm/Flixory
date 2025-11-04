f7d834a94003937100421499cb3f760cc6581d10
flixory2025-lgtm <flixory2025@gmail.com>
2025-11-04 08:00:42 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,16 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 import { FirebaseProvider } from "@/components/firebase-provider"
 
 const _geist = Geist({ subsets: ["latin"] })
-const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "FLIXORY",
+  description: "a streaming movie platform",
+  generator: "v0 Dev",
 }
 
 export default function RootLayout({
