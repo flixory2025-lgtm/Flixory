3ea14b016542ee30d15163932a6763551bd2cba8
flixory2025-lgtm <flixory2025@gmail.com>
2025-11-04 08:02:04 +0600

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,25 +1,35 @@
-import { initializeApp } from "firebase/app"
-import { getAuth, type Auth } from "firebase/auth"
-import { getFirestore, type Firestore } from "firebase/firestore"
-import { getStorage, type FirebaseStorage } from "firebase/storage"
+let app: any = null
+let auth: any = null
+let db: any = null
+let storage: any = null
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAQB61QWus2fMNS5FNU4nLZCKnU5qONBY4",
-  authDomain: "flixory-74271.firebaseapp.com",
-  databaseURL: "https://flixory-74271-default-rtdb.firebaseio.com",
-  projectId: "flixory-74271",
-  storageBucket: "flixory-74271.firebasestorage.app",
-  messagingSenderId: "292608615173",
-  appId: "1:292608615173:web:30a2988703b506e4424d6c",
-  measurementId: "G-8ZF9KF2BG3",
+try {
+  const { initializeApp } = require("firebase/app")
+  const { getAuth } = require("firebase/auth")
+  const { getFirestore } = require("firebase/firestore")
+  const { getStorage } = require("firebase/storage")
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAQB61QWus2fMNS5FNU4nLZCKnU5qONBY4",
+    authDomain: "flixory-74271.firebaseapp.com",
+    databaseURL: "https://flixory-74271-default-rtdb.firebaseio.com",
+    projectId: "flixory-74271",
+    storageBucket: "flixory-74271.firebasestorage.app",
+    messagingSenderId: "292608615173",
+    appId: "1:292608615173:web:30a2988703b506e4424d6c",
+    measurementId: "G-8ZF9KF2BG3",
+  }
+
+  // Initialize Firebase
+  app = initializeApp(firebaseConfig)
+
+  // Initialize Firebase services
+  auth = getAuth(app)
+  db = getFirestore(app)
+  storage = getStorage(app)
+} catch (error) {
+  console.log("[v0] Firebase initialization will happen on client-side")
 }
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
-
-// Initialize Firebase services
-export const auth: Auth = getAuth(app)
-export const db: Firestore = getFirestore(app)
-export const storage: FirebaseStorage = getStorage(app)
-
+export { app, auth, db, storage }
 export default app
